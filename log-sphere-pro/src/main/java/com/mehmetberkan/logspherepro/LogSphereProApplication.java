84a40e8e4018b528c6214388a2a6bc5dd8065461
Mehmet Berkan <mehmetberkangorel@gmail.com>
2025-01-08 00:17:06 +0300

    Initial commit

---
+++ log-sphere-pro/src/main/java/com/mehmetberkan/logspherepro/LogSphereProApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.logspherepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSphereProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogSphereProApplication.class, args);
+	}
+
+}
---
+++ log-sphere-pro/src/test/java/com/mehmetberkan/logspherepro/LogSphereProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.logspherepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSphereProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
