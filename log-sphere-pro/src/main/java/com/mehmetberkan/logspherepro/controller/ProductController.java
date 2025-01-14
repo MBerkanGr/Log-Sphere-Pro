b3f6644194dbbfa4b2f80d0af80d8ac9b5b98eee
Mehmet Berkan <mehmetberkangorel@gmail.com>
2025-01-14 20:22:51 +0300

    Update logback-spring

--- log-sphere-pro/src/main/java/com/mehmetberkan/logspherepro/controller/ProductController.java
+++ log-sphere-pro/src/main/java/com/mehmetberkan/logspherepro/controller/ProductController.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,8 +32,7 @@ public class ProductController {
     }
 
     @GetMapping("/{productCode}")
-    public ResponseEntity<Product> getByProductCode(@RequestParam("productCode") String productCode) {
-
+    public ResponseEntity<Product> getByProductCode(@PathVariable("productCode") String productCode) {
         logger.info("Started GetByProductCode, Product Code : {}", productCode);
         return ResponseEntity.ok(productService.getByProductCode(productCode));
     }
